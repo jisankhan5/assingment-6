f42e4b07a890b75cc5a7cf117afe07130eb1037d
jisankhan5 <jk1483777@gmail.com>
2025-09-07 12:51:42 +0600

    added more functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const categoryContainer = document.getElementById("categoryContainer");
-
+const planContainer = document.getElementById("planContainer");
 const loadCategory = () => {
   fetch("https://openapi.programming-hero.com/api/categories")
     .then((res) => res.json())
@@ -14,7 +14,7 @@ const loadCategory = () => {
 const showCategory = (categories) => {
   categories.forEach((cat) => {
     categoryContainer.innerHTML += `
-        <li class="cursor-pointer w-full hover:bg-[#2ea754] p-2 rounded-md hover:transition duration-500 hover:text-white ">
+        <li id="${cat.id}" class="cursor-pointer w-full hover:bg-[#2ea754] p-2 rounded-md hover:transition duration-500 hover:text-white ">
                 ${cat.category_name}
               </li>
         `;
@@ -22,6 +22,7 @@ const showCategory = (categories) => {
 
   categoryContainer.addEventListener("click", (e) => {
     const allLi = document.querySelectorAll("li");
+
     allLi.forEach((li) => {
       li.classList.remove("bg-green-700");
       li.classList.remove("text-white");
@@ -30,8 +31,38 @@ const showCategory = (categories) => {
     if (e.target.localName === "li") {
       e.target.classList.add("bg-green-700");
       e.target.classList.add("text-white");
+      plantsByCategory(e.target.id);
     }
   });
 };
 
+const plantsByCategory = (plantId) => {
+  fetch(`https://openapi.programming-hero.com/api/category/${plantId}`)
+    .then((res) => res.json())
+    .then((data) => {
+      showPlantByCategory(data.plants);
+    });
+};
+
+const showPlantByCategory = (plants) => {
+  console.log(plants);
+  planContainer.innerHTML = "";
+  plants.forEach((plant) => {
+    planContainer.innerHTML += `
+    <div class="bg-white shadow-lg hover:transition hover:translate-2 duration-300  space-y-3 p-4 rounded-lg h-[530px]">
+    <img class="rounded-lg h-[250px] w-full" src="${plant.image}">
+      <h1 class="font-semibold text-lg cursor-pointer">${plant.name}</h1>
+      <p class="text-gray-600">${plant.description}</p>
+      <div class="flex justify-between">
+        <button class="btn bg-[#DCFCE7] rounded-full text-[#15803D]">${plant.category}</button>
+        <p><i class="fa-solid fa-bangladeshi-taka-sign"></i>${plant.price}</p>
+      </div>
+    <button class="btn w-full rounded-full text-white bg-green-700 p-2">Add to Cart</button>
+    </div>
+
+    `;
+  });
+};
+
 loadCategory();
+
