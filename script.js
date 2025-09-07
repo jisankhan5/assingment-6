50bcca5bafe2535ad5e4f25b2612e4c674bd9000
jisankhan5 <jk1483777@gmail.com>
2025-09-07 14:57:30 +0600

    added all card functionality

--- script.js
+++ script.js
@@ -45,7 +45,6 @@ const plantsByCategory = (plantId) => {
 };
 
 const showPlantByCategory = (plants) => {
-  console.log(plants);
   planContainer.innerHTML = "";
   plants.forEach((plant) => {
     planContainer.innerHTML += `
@@ -70,15 +69,28 @@ const loadAllCategorys = () => {
   fetch("https://openapi.programming-hero.com/api/plants")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.plants);
-      
-    })
-   
+      const cardAll = data.plants;
+      showAllCategor(cardAll);
+    });
 };
-const showAllCategor = (el) => {
-  planContainer.innerHTML += `
-       
-        `;
-};
+const showAllCategor = (cardAll) => {
+  console.log(cardAll);
 
-loadAllCategorys()
+  cardAll.forEach((card) => {
+    planContainer.innerHTML += `
+       <div class="bg-white shadow-lg hover:transition hover:translate-2 duration-300  space-y-3 p-4 rounded-lg h-[530px]">
+    <img class="rounded-lg h-[250px] w-full" src="${card.image}">
+      <h1 class="font-semibold text-lg cursor-pointer">${card.name}</h1>
+      <p class="text-gray-600">${card.description}</p>
+      <div class="flex justify-between">
+        <button class="btn bg-[#DCFCE7] rounded-full text-[#15803D]">${card.category}</button>
+        <p><i class="fa-solid fa-bangladeshi-taka-sign"></i>${card.price}</p>
+      </div>
+    <button class="btn w-full rounded-full text-white bg-green-700 p-2">Add to Cart</button>
+    </div>
+        `;
+  });
+};
+loadAllCategorys();
+
+
